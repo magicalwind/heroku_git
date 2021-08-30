a93be609572c7bd609350074289d9f833b7e192b
magicalwind <ltb_magical@hotmail.com>
2021-08-29 21:42:10 -0700

    fixed_issue

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
 
-from db import db
 
 from security import authenticate, identity
 from resources.user import UserRegister
@@ -17,9 +16,6 @@ app.secret_key = 'jose'
 api = Api(app)
 
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
 
 jwt = JWT(app, authenticate, identity)  # /auth
@@ -31,5 +27,6 @@ api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
+    from db import db
     db.init_app(app)
     app.run(port=5000, debug=True)
