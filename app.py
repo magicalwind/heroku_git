acdf85988c8867d8972416e3ce0d571522022cb4
magicalwind <ltb_magical@hotmail.com>
2021-08-29 22:50:28 -0700

    bug fixed

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///datab.db')   # , set the db path, for now, the path of db is the root of app.py
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://eblrusjmnmquxu:3991d130b10cbb6f3b62c7e00fb726eeafc231f3e5c3a2ed1ae480eab1d5bfe3@ec2-18-209-153-180.compute-1.amazonaws.com:5432/d7s5a73883li2a'   # , set the db path, for now, the path of db is the root of app.py
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'jose'
